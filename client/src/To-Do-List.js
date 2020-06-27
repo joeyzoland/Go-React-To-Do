5b91cd853282b6d06fac2b8282d5220817ade630
Joseph Zoland <joeyzoland@gmail.com>
2020-06-26 20:51:39 -0700

    Added React stuff

---
+++ client/src/To-Do-List.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
+
+let endpoint = "http://localhost:8080";
+
+class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: "",
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    this.getTask();
+  }
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onSubmit = () => {
+    let { task } = this.state;
+    //console.log("this.state.task is " + this.state.task)
+    if (task) {
+      axios
+        .post(
+          endpoint + "/api/task",
+          {
+            task
+          },
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+          }
+        )
+        .then(res => {
+          this.getTask();
+          this.setState({
+            task: ""
+          });
+          //console.log(res)
+        })
+    }
+  };
+
+  
+}
