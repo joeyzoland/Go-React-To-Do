33971d51140389c8d78211ecbda00a48cf7b232e
Joseph Zoland <joeyzoland@gmail.com>
2020-06-27 15:06:23 -0700

    Adding more frontend work

--- client/src/To-Do-List.js
+++ client/src/To-Do-List.js
@@ -50,5 +50,83 @@ class ToDoList extends Component {
     }
   };
 
-  
+  getTask = () => {
+    axios.get(endpoint + "/api/task").then(res => {
+      //console.log(res)
+      if (res.data){
+        this.setState({
+          items: res.data.map( item => {
+            let color = "yellow";
+            if (item.status) {
+              color = "green";
+            }
+            return (
+              <Card key={item._id} color={color} fluid>
+                <Card.Content>
+                  <Card.Header textAlign="left">
+                    <div style={{ wordWrap: "break-word" }}>{item.task}</div>
+                  </Card.Header>
+
+                  <Card.Meta textAlign="right">
+                    <Icon
+                      name="check circle"
+                      color="green"
+                      onClick={() => this.updateTask(item._id)}
+                    />
+                    <span style={{ paddingRight:10 }}>Done</span>
+                    <Icon
+                      name="undo"
+                      color="yellow"
+                      onClick={() => this.undoTask(item._id)}
+                    />
+                    <span style={{ paddingRight:10 }}>Undo</span>
+                    <Icon
+                      name="delete"
+                      color="red"
+                      onClick{() => this.deleteTask(item._id)}
+                    />
+                    <span style={{ paddingRight:10 }}>Delete</span>
+                  </Card.Meta>
+                </Card.Content>
+              </Card>
+            );
+          })
+        });
+      } else {
+        this.setState({
+          items: []
+        });
+      }
+    });
+  };
+
+  updateTask = id => {
+    axios
+      .put(endpoint + "/api/task/" + id, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+      .then(res => {
+        //console.log(res);
+        this.getTask();
+      });
+  };
+
+  undoTask = id => {
+    axios
+      .put(endpoint + "/api/undoTask/" + id, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+      .then(res => {
+        //console.log(res);
+        this.getTask();
+      });
+  };
+
+
+
+
 }
