c26c7029049f6e7ac1b9224a107c7a7bf2bb20c3
Joseph Zoland <joeyzoland@gmail.com>
2020-07-01 21:23:23 -0700

    Removed extraneous id property

--- server/models/models.go
+++ server/models/models.go
@@ -1,9 +1,9 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+//import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type ToDoList struct{
-  ID     primitive.ObjectID `json:"_ids omitempty" bson:"_ids, omitempty"`
+  //ID     primitive.ObjectID `json:"_ids omitempty" bson:"_ids, omitempty"`
   Task   string `json:"task, omitempty"`
   Status bool `'json:"status, omitempty"`
 }
